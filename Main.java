7bbc2e55aa159520443112473140dde6b1ee3d1b
Skoutelis Konstantinos <skouteliskonstantinos@gmail.com>
2021-05-12 20:40:08 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("If you see this message, then the build went successfully");
+        System.out.println("At " + LocalDateTime.now());
+    }
+}
